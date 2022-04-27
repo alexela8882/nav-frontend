1edd9dddfed8bb166e189cc16a5757a0296b0af2
alexela8882 <alexela8882@gmail.com>
2022-04-27 15:35:46 +0800

    table added

--- pages/nav/index.vue
+++ pages/nav/index.vue
@@ -45,7 +45,50 @@ export default {
         this.data = request
         this.url = null // reset
         console.log(request)
+
+        this.generateDynamicTable()
       }, 2000)
+    },
+
+    generateDynamicTable () {
+      const data = this.data.value
+      // EXTRACT VALUE FOR HTML HEADER. 
+      var col = [];
+      for (var i = 0; i < data.length; i++) {
+        for (var key in data[i]) {
+          if (col.indexOf(key) === -1) {
+            col.push(key);
+          }
+        }
+      }
+
+      // CREATE DYNAMIC TABLE.
+      var table = document.createElement("table");
+      table.setAttribute("class", "table-auto");
+
+      // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+      var tr = table.insertRow(-1);                   // TABLE ROW.
+
+      for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+      }
+
+      // ADD JSON DATA TO THE TABLE AS ROWS.
+      for (var i = 0; i < data.length; i++) {
+        tr = table.insertRow(-1);
+        for (var j = 0; j < col.length; j++) {
+          var tabCell = tr.insertCell(-1);
+          tabCell.innerHTML = data[i][col[j]];
+        }
+      }
+
+      // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+      var divContainer = document.getElementById("showData");
+      divContainer.innerHTML = "";
+      divContainer.appendChild(table);
     }
   }
 }
@@ -121,9 +164,11 @@ export default {
 
     <br class="my-5">
 
-    <pre>{{ data ? data : 'Loading. Please wait...' }}</pre>
+    <p>{{ data ? "Data succesfully fetched." : 'Loading. Please wait...' }}</p>
 
-    <button :disabled="!url" class="btn m-3 text-sm" @click="getURL()">Get data</button>
+    <p id='showData' class="my-5"></p>
+
+    <button :disabled="!url || data == null" class="btn m-3 text-sm" @click="getURL()">Get data</button>
 
     <NuxtLink
       class="btn m-3 text-sm"
