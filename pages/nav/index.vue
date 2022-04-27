e996e9212a38df3ddcce1f34c77630496eddd523
alexela8882 <alexela8882@gmail.com>
2022-04-27 14:32:51 +0800

    In the beginning

---
+++ pages/nav/index.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data () {
+    return {
+      data: "Test"
+    }
+  },
+
+  created () {
+    this.initialize()
+  },
+
+  methods: {
+    async initialize() {
+      this.data = null // reset
+
+      let request = null
+
+      var xhttp = new XMLHttpRequest()
+      xhttp.open("GET", "http://LEKZPC:7048/DynamicsNAV100/ODataV4/Company('CRONUS%20International%20Ltd.')/powerbifinance", true);
+      xhttp.withCredentials = true
+      xhttp.send("Username: Admin", "Password: acf0106")
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === XMLHttpRequest.DONE) {
+          if (xhttp.status === 200) {
+            const response = JSON.parse(xhttp.response)
+            console.log(response.value)
+            request = response.value
+          } else console.log('There was a problem with the request.')
+        }
+      }
+
+      setTimeout( () => {
+        this.data = request
+        console.log(request)
+      }, 2000)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <strong>DATA:</strong>
+    <pre>{{ data ? data : 'Loading. Please wait...' }}</pre>
+  </div>
+</template>
