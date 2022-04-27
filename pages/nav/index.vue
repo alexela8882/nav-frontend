6e1b014cf60536a69d32edecd329cb08eeb7cadd
alexela8882 <alexela8882@gmail.com>
2022-04-27 15:10:55 +0800

    input boxes applied for development

--- pages/nav/index.vue
+++ pages/nav/index.vue
@@ -2,7 +2,10 @@
 export default {
   data () {
     return {
-      data: "Test"
+      data: "Your data will be displayed here.",
+      url: null,
+      username: null,
+      password: null
     }
   },
 
@@ -11,27 +14,36 @@ export default {
   },
 
   methods: {
-    async initialize() {
+    initialize () {
+      console.log("Components created.")
+    },
+
+    getURL () {
       this.data = null // reset
 
       let request = null
 
       var xhttp = new XMLHttpRequest()
-      xhttp.open("GET", "http://LEKZPC:7048/DynamicsNAV100/ODataV4/Company('CRONUS%20International%20Ltd.')/powerbifinance", true);
+      xhttp.open("GET", this.url, true);
       xhttp.withCredentials = true
-      xhttp.send("Username: Admin", "Password: acf0106")
+      xhttp.send(`Username: ${this.username}`, `Password: ${this.password}`)
       xhttp.onreadystatechange = function () {
         if (xhttp.readyState === XMLHttpRequest.DONE) {
           if (xhttp.status === 200) {
             const response = JSON.parse(xhttp.response)
             console.log(response.value)
-            request = response.value
-          } else console.log('There was a problem with the request.')
+            request = response
+          } else {
+            const msg = 'There was a problem with the request. Please try again.'
+            console.log(msg)
+            request = msg
+          }
         }
       }
 
       setTimeout( () => {
         this.data = request
+        this.url = null // reset
         console.log(request)
       }, 2000)
     }
@@ -59,8 +71,55 @@ export default {
 
     <br>
 
-    <strong>DATA:</strong>
+    <div class="d-flex flex-column">
+      <input
+        v-model="url"
+        placeholder="Enter your URL here..."
+        type="text"
+        autocomplete="false"
+        p="x-4 y-2"
+        m="t-5"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+      >
+
+      <input
+        v-model="username"
+        placeholder="Enter your username"
+        type="text"
+        autocomplete="false"
+        p="x-4 y-2"
+        m="t-5"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+      >
+
+      <input
+        v-model="password"
+        placeholder="Enter your password"
+        type="password"
+        autocomplete="false"
+        p="x-4 y-2"
+        m="t-5"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+      >
+    </div>
+
+    <br class="my-5">
+
     <pre>{{ data ? data : 'Loading. Please wait...' }}</pre>
+
+    <button class="btn m-3 text-sm" @click="getURL()">Get data</button>
 
     <NuxtLink
       class="btn m-3 text-sm"
